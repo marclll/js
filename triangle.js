fbe3517d73c49ff3204347f46e627898992ad8ca
marclll <sgmarcsg@gmail.com>
2018-07-07 17:52:00 +0200

    triangle

--- triangle.js
+++ triangle.js
@@ -1,48 +1,12 @@
 window.onload=function(){
-	setInterval(move,3200)
-}
-
-function move(){
-	setTimeout(move1,1000);
-	setTimeout(move2,1000);
-	setTimeout(move3,1000);
-	setTimeout(move4,1000);
-	setTimeout(move5,1000);
-	setTimeout(move6,1000);
-	setTimeout(move7,1000);
-}
-
-function move1(){
 	var it=document.getElementById("doit");
-	it.style.transform="translate(2500px,1200px)";
-}
-
-function move2(){
-	var it=document.getElementById("doit");
-	it.style.transform="translate(25%,75%)";
-}
-
-function move3(){
-	var it=document.getElementById("doit");
-	it.style.transform="translate(78%,78%)";
-}
-
-function move4(){
-	var it=document.getElementById("doit");
-	it.style.transform="translate(0px,200px)";
-}
-
-function move5(){
-	var it=document.getElementById("doit");
-	it.style.transform="translate(0px,200px)";
-}
-
-function move6(){
-	var it=document.getElementById("doit");
-	it.style.transform="translate(0px,200px)";
-}
-
-function move7(){
-	var it=document.getElementById("doit");
-	it.style.transform="translate(0px,200px)";
+	setInterval(function(){it.style.transform = "translate(10vw,50vh)";},400);
+	setInterval(function(){it.style.transform = "translate(20vw,10vh)";},540);
+	setInterval(function(){it.style.transform = "translate(70vw,30vh)";},620);
+	setInterval(function(){it.style.transform = "translate(50vw,25vh)";},560);
+	setInterval(function(){it.style.transform = "translate(10vw,40vh)";},600);
+	setInterval(function(){it.style.transform = "translate(40vw,50vh)";},580);
+	setInterval(function(){it.style.transform = "translate(30vw,70vh)";},500);
+	setInterval(function(){it.style.transform = "translate(50vw,60vh)";},820);
+	setInterval(function(){it.style.transform = "translate(80vw,80vh)";},680);
 }
